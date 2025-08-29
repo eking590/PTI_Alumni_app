76cd2fbd9aea61a58dd7bf5730a8d467b50c3287
ebor odiri kingsley <59615834+eking590@users.noreply.github.com>
2025-08-29 16:35:12 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,9 +3,10 @@ import { config } from "dotenv";
 
 config() 
 
-const Mongoose = process.env.MONGO_URL || mongodb+srv://ogehebor:wsWhMDGJUQthQlxP@cloud9.vgm2e.mongodb.net/?retryWrites=true&w=majority&appName=cloud9; 
+const Mongoose = process.env.MONGO_URL || "mongodb+srv://ogehebor:wsWhMDGJUQthQlxP@cloud9.vgm2e.mongodb.net/?retryWrites=true&w=majority&appName=cloud9"; 
 
 export const db = mongoose.connect(Mongoose, { })
 .then(() => console.log('Connected to PTI Alumni database...')) 
 
 .catch(err => console.log('Could not connect to PTI Alumni database...'))
+
