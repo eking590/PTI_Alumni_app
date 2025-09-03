b52fd213f17b46f462708ff1eab70c2312def038
ebor odiri kingsley <59615834+eking590@users.noreply.github.com>
2025-09-03 20:50:37 +0100

    Update generateToken.js

--- middlewares/generateToken.js
+++ middlewares/generateToken.js
@@ -1,1 +1,10 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (alumni) => {
+  return jwt.sign(
+    { id: alumni._id, email: alumni.email }, 
+    process.env.JWT_SECRET, 
+    { expiresIn: "7d" } // token valid for 7 days
+  );
+};
 
