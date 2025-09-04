0a06b5ac3b00e58d68bfb7f6bd992fb32cfa8614
ebor odiri kingsley <59615834+eking590@users.noreply.github.com>
2025-09-04 16:28:01 +0100

    Update adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,40 +1,11 @@
 import Admin from '../models/Admin.js';
 import Alumni from '../models/Alumni.js';
+import { generateToken } from '../middlewares/jwt.js';
 
 export const getLogin = (req, res) => {
   return res.render('pages/admin/login', { title: 'Admin Login',  error: null });
 };
 
-// export const postLogin = async (req, res) => {
-//   const { username, password } = req.body;
-  
-//   try {
-//     const admin = await Admin.findOne({ username });
-//     if (!admin) {
-//       return res.status(401).render('pages/login', { 
-//         title: 'Admin Login',
-//         error: 'Invalid credentials' 
-//       });
-//     }
-
-//     const isMatch = await admin.comparePassword(password);
-//     if (!isMatch) {
-//       return res.status(401).render('pages/login', { 
-//         title: 'Admin Login',
-//         error: 'Invalid credentials' 
-//       });
-//     }
-
-//     req.session.admin = admin;
-//     res.redirect('/dashboard');
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).render('pages/login', { 
-//       title: 'Admin Login',
-//       error: 'Server error' 
-//     });
-//   }
-// };
 
 export const postLogin = async (req, res) => {
   const { username, password } = req.body;
@@ -52,7 +23,11 @@ export const postLogin = async (req, res) => {
       return res.redirect('/login');
     }
 
-    req.session.admin = admin;
+    const token = generateToken(admin, 'admin');
+
+    // Save token in cookie
+    res.cookie('token', token, { httpOnly: true, secure: false });
+
     return res.redirect('/dashboard');
   } catch (err) {
     console.error(err);
@@ -62,27 +37,28 @@ export const postLogin = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-  req.session.destroy(err => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Server error');
-    }
-    res.redirect('/login');
-  })
+  res.clearCookie('token');
+  res.redirect('/login');
 };  
 
 
 export const getDashboard = async (req, res) => {
-  try {
-    const alumniCount = await Alumni.countDocuments();
-    const recentAlumni = await Alumni.find().sort({ createdAt: -1 }).limit(5);
-
-    res.render('pages/admin/dashboard', {
-      title: 'Dashboard',
+  if (req.user.type !== 'admin') {
+    return res.redirect('/login');
+  }
+   
+   try {
+  const alumniCount = await Alumni.countDocuments();
+  const recentAlumni = await Alumni.find().sort({ createdAt: -1 }).limit(5);
+  const admin = await Admin.findById(req.user.id);
+  res.render('pages/admin/dashboard', { 
+       title: 'Dashboard',
       alumniCount,
       recentAlumniCount: recentAlumni.length,
-      recentAlumni
+      recentAlumni,
+      admin 
     });
+
   } catch (err) {
     console.error(err);
     res.status(500).render('pages/error', {
